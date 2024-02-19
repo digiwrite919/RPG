cde43baaea0b7de82684bc501d1716d6ad30c769
digiwrite919 <159510585+digiwrite919@users.noreply.github.com>
2024-02-19 14:56:48 +0900

    課題提出

---
+++ src/humans/Fighter.java
@@ -0,0 +1,15 @@
+package humans;
+
+import bases.Human;
+import utils.Dice;
+
+public class Fighter extends Human {
+
+    // コンストラクタ
+    public Fighter(String name, String weapon) {
+        super(name, weapon);
+        this.hp = Dice.get(240, 300);
+        this.offensive = Dice.get(17, 23);
+    }
+
+}
